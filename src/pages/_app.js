3c1e48bf4cb49bc2db13a3b1d347d393db6ead29
Hasitha Chamikara <72026605+sevenhc@users.noreply.github.com>
2025-06-05 12:16:46 +0530

    Fix app initialization error

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider session={pageProps.session}>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
   )
 }
+
+export default MyApp;
